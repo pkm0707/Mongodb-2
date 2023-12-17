d49e5a7811c4f489625b257b3fde556314260b43
pkm0707 <mpraveenkumar@student.tce.edu>
2023-12-17 09:19:51 +0530

    final commit

---
+++ answers.js
@@ -0,0 +1,326 @@
+// 1.Find all the topics and tasks which are thought in the month of October
+[{
+    "_id" : ObjectId("657e68f528c48da93e141775"),
+    "id" : "51",
+    "name" : "task3",
+    "topic_name" : "topic3",
+    "user_id" : "5",
+    "date" : "17/10/2023",
+    "status" : "In Progress",
+    "matches" : [ ]
+},
+{
+    "_id" : ObjectId("657e68f528c48da93e141776"),
+    "id" : "52",
+    "name" : "task4",
+    "topic_name" : "topic4",
+    "user_id" : "5",
+    "date" : "18/10/2023",
+    "status" : "In Progress",
+    "matches" : [ ]
+},
+{
+    "_id" : ObjectId("657e68f528c48da93e141777"),
+    "id" : "53",
+    "name" : "task5",
+    "topic_name" : "topic5",
+    "user_id" : "5",
+    "date" : "19/10/2023",
+    "status" : "In Progress",
+    "matches" : [ ]
+},
+{
+    "_id" : ObjectId("657e68f528c48da93e141778"),
+    "id" : "54",
+    "name" : "task6",
+    "topic_name" : "topic6",
+    "user_id" : "5",
+    "date" : "20/10/2023",
+    "status" : "In Progress",
+    "matches" : [ ]
+},
+{
+    "_id" : ObjectId("657e68f528c48da93e141779"),
+    "id" : "55",
+    "name" : "task7",
+    "topic_name" : "topic7",
+    "user_id" : "5",
+    "date" : "23/10/2023",
+    "status" : "Completed",
+    "matches" : [ ]
+},
+{
+    "_id" : ObjectId("657e68f528c48da93e14177a"),
+    "id" : "56",
+    "name" : "task8",
+    "topic_name" : "topic8",
+    "user_id" : "5",
+    "date" : "24/10/2023",
+    "status" : "Completed",
+    "matches" : [ ]
+},
+{
+    "_id" : ObjectId("657e68f528c48da93e14177b"),
+    "id" : "57",
+    "name" : "task9",
+    "topic_name" : "topic9",
+    "user_id" : "5",
+    "date" : "25/10/2023",
+    "status" : "In Progress",
+    "matches" : [ ]
+},
+{
+    "_id" : ObjectId("657e68f528c48da93e14177c"),
+    "id" : "58",
+    "name" : "task10",
+    "topic_name" : "topic10",
+    "user_id" : "5",
+    "date" : "26/10/2023",
+    "status" : "In Progress",
+    "matches" : [ ]
+},
+{
+    "_id" : ObjectId("657e68f528c48da93e14177d"),
+    "id" : "59",
+    "name" : "task11",
+    "topic_name" : "topic11",
+    "user_id" : "5",
+    "date" : "29/10/2023",
+    "status" : "Completed",
+    "matches" : [ ]
+},
+{
+    "_id" : ObjectId("657e68f528c48da93e14177e"),
+    "id" : "60",
+    "name" : "task12",
+    "topic_name" : "topic12",
+    "user_id" : "5",
+    "date" : "30/10/2023",
+    "status" : "Completed",
+    "matches" : [ ]
+}]
+
+// 2.Find all the company drives which appeared between 15 oct-2020 and 31-oct-2020
+// No Company drives from my inputed data
+
+// 3.Find all the company drives and students who are appeared for the placement.
+[{
+    "_id" : ObjectId("657e690928c48da93e141780"),
+    "id" : "2",
+    "name" : "drive2",
+    "user_id" : "1",
+    "conducted_at" : "25/10/2023",
+    "status" : "Attended"
+},
+{
+    "_id" : ObjectId("657e690928c48da93e141781"),
+    "id" : "3",
+    "name" : "drive3",
+    "user_id" : "1",
+    "conducted_at" : "30/10/2023",
+    "status" : "Attended"
+},
+{
+    "_id" : ObjectId("657e690928c48da93e141782"),
+    "id" : "4",
+    "name" : "drive1",
+    "user_id" : "2",
+    "conducted_at" : "15/10/2023",
+    "status" : "Attended"
+},
+{
+    "_id" : ObjectId("657e690928c48da93e141784"),
+    "id" : "6",
+    "name" : "drive3",
+    "user_id" : "2",
+    "conducted_at" : "30/10/2023",
+    "status" : "Attended"
+},
+{
+    "_id" : ObjectId("657e690928c48da93e141786"),
+    "id" : "8",
+    "name" : "drive2",
+    "user_id" : "3",
+    "conducted_at" : "25/10/2023",
+    "status" : "Attended"
+},
+{
+    "_id" : ObjectId("657e690928c48da93e14178a"),
+    "id" : "12",
+    "name" : "drive3",
+    "user_id" : "4",
+    "conducted_at" : "30/10/2023",
+    "status" : "Attended"
+},
+{
+    "_id" : ObjectId("657e690928c48da93e14178c"),
+    "id" : "14",
+    "name" : "drive2",
+    "user_id" : "5",
+    "conducted_at" : "25/10/2023",
+    "status" : "Attended"
+}]
+
+// 4.Find the number of problems solved by the user in codekata
+[{
+    "_id" : ObjectId("657e664a28c48da93e14172c"),
+    "id" : "1",
+    "name" : "Praveen Kumar M",
+    "coursename" : "MERN-Stack",
+    "mentorname" : "Sangeetha",
+    "email" : "mpraveenkumar@gmail.com",
+    "phone" : "9854217781",
+    "Problems_solved" : [
+        {
+            "_id" : "program9"
+        },
+        {
+            "_id" : "program2"
+        },
+        {
+            "_id" : "program5"
+        },
+        {
+            "_id" : "program1"
+        }
+    ]
+},
+{
+    "_id" : ObjectId("657e664a28c48da93e14172d"),
+    "id" : "2",
+    "name" : "Mithra S",
+    "coursename" : "MERN-Stack",
+    "mentorname" : "Sangeetha",
+    "email" : "mithra@gmail.com",
+    "phone" : "9654812345",
+    "Problems_solved" : [
+        {
+            "_id" : "program1"
+        },
+        {
+            "_id" : "program9"
+        }
+    ]
+},
+{
+    "_id" : ObjectId("657e664a28c48da93e14172e"),
+    "id" : "3",
+    "name" : "Vivek Kumar P",
+    "coursename" : "MERN-Stack",
+    "mentorname" : "Sangeetha1",
+    "email" : "vivekkumar@gmail.com",
+    "phone" : "7854123546",
+    "Problems_solved" : [
+        {
+            "_id" : "program4"
+        }
+    ]
+},
+{
+    "_id" : ObjectId("657e664a28c48da93e14172f"),
+    "id" : "4",
+    "name" : "Pavithra V",
+    "coursename" : "MERN-Stack",
+    "mentorname" : "Sangeetha1",
+    "email" : "pavithra@gmail.com",
+    "phone" : "8456218542",
+    "Problems_solved" : [
+        {
+            "_id" : "program5"
+        }
+    ]
+},
+{
+    "_id" : ObjectId("657e664a28c48da93e141730"),
+    "id" : "5",
+    "name" : "Keerthana",
+    "coursename" : "MERN-Stack",
+    "mentorname" : "Sangeetha",
+    "email" : "keerthana@gmail.com",
+    "phone" : "8845615784",
+    "Problems_solved" : [
+        {
+            "_id" : "program5"
+        },
+        {
+            "_id" : "program9"
+        }
+    ]
+}]
+
+// 5.Find all the mentors with who has the mentee's count more than 15
+[{
+    "_id" : ObjectId("657e692928c48da93e14178e"),
+    "id" : "1",
+    "name" : "Sangeetha",
+    "mentee_user_id" : "1",
+    "mentee_name" : "Praveeen Kumar M",
+    "Mentors" : [ ]
+},
+{
+    "_id" : ObjectId("657e692928c48da93e14178f"),
+    "id" : "2",
+    "name" : "Sangeetha",
+    "mentee_user_id" : "2",
+    "mentee_name" : "Mithra S",
+    "Mentors" : [
+        {
+            "_id" : "2"
+        }
+    ]
+},
+{
+    "_id" : ObjectId("657e692928c48da93e141790"),
+    "id" : "3",
+    "name" : "Sangeetha",
+    "mentee_user_id" : "5",
+    "mentee_name" : "Vivek Kumar P",
+    "Mentors" : [
+        {
+            "_id" : "3"
+        }
+    ]
+},
+{
+    "_id" : ObjectId("657e692928c48da93e141791"),
+    "id" : "4",
+    "name" : "sharjeel",
+    "mentee_user_id" : "3",
+    "mentee_name" : "Pavithra V",
+    "Mentors" : [
+        {
+            "_id" : "4"
+        }
+    ]
+},
+{
+    "_id" : ObjectId("657e692928c48da93e141792"),
+    "id" : "5",
+    "name" : "sharjeel",
+    "mentee_user_id" : "4",
+    "mentee_name" : "Keerthana",
+    "Mentors" : [
+        {
+            "_id" : "5"
+        }
+    ]
+}]
+
+// 6.Find the number of users who are absent and task is not submitted  between 15 oct-2020 and 31-oct-2020
+[{ "Absentees" : 8 }]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
